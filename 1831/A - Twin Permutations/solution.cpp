3c5cd026c22b25e29d4b34a51be8060362fe777e
Aditya Gupta <aditya.g24@iiits.in>
2026-06-13 19:32:51 +0530

    Add Twin Permutations [A] from Codeforces

---
+++ 1831/A - Twin Permutations/solution.cpp
@@ -0,0 +1,21 @@
+    #include <bits/stdc++.h>
+    using namespace std;
+ 
+    int main() {
+        int t;
+        cin >> t;
+        while (t--) {
+            int n;
+            cin >> n;
+            vector<int> a(n);
+            for (int i = 0; i < n; i++) cin >> a[i];
+ 
+            int x = n + 1;
+ 
+            for(int i : a){
+                cout<<x - i << " ";
+            }
+            cout<<endl;
+        }
+        return 0;
+    }
