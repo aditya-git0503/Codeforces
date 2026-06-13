ac715ef05e7c9f37adc4883c0608554285ef5cec
Aditya Gupta <aditya.g24@iiits.in>
2026-06-13 16:24:14 +0530

    Add Buttons [A] from Codeforces

---
+++ 1858/A - Buttons/solution.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        int a,b,c;
+        cin >> a >> b >> c;
+ 
+        if(c&1){
+            if(b>a){
+                cout<<"Second
+";
+            } else {
+                cout<<"First
+";
+            }
+        } else {
+            if(a>b){
+                cout<<"First
+";
+            } else {
+                cout<<"Second
+";
+            }
+        }
+    }
+ 
+    return 0;
+}
